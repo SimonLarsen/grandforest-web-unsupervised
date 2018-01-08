aa497bee15fb53d0e57194bc822ff05f79a86fe0
Simon Larsen <simonhffh@gmail.com>
2018-01-08 15:30:52 +0100

    Added Reactome gene set enrichment

--- install.R
+++ install.R
@@ -6,6 +6,7 @@ biocLite('ComplexHeatmap', ask=FALSE)
 biocLite('org.Hs.eg.db', ask=FALSE)
 biocLite('DOSE', ask=FALSE)
 biocLite('clusterProfiler', ask=FALSE)
+biocLite("ReactomePA", ask=FALSE)
 
 devtools::install_github('AnalytixWare/ShinySky')
 devtools::install_github('SimonLarsen/grandforest', auth_token='edb93ddf877c24e3e71a66fdb96092c2e96bed82')
