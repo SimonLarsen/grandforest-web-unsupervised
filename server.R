9e33078b65a299a27cf5eba10f6341e14cabb2c7
Simon Larsen <simonhffh@gmail.com>
2018-05-23 16:18:59 +0200

    Check example data species earlier

--- server.R
+++ server.R
@@ -145,15 +145,15 @@ shinyServer(function(input, output, session) {
       return()
     }
 
+    if(input$useExampleData && input$species != "human") {
+      alert("Please set species to \"Homo sapiens\" when using example data.")
+      return()
+    }
+
     withProgress(value=0, message = "Parsing data", {
       setProgress(value=0.1, detail="Parsing expression data")
 
       if(input$useExampleData) {
-        if(input$species != "human") {
-          alert("Please set species to \"Homo sapiens\" when using example data.")
-          return()
-        }
-
         D <- readRDS(EXAMPLE_DATA_PATH)
         clusterVar <- EXAMPLE_DATA_CLUSTERVAR
         hasSurvival <- TRUE
